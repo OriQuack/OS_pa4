ac48df547ad157dd47af5da0e33fb40453d7c890
OriQuack <kimdoh0423@naver.com>
2024-05-23 00:13:58 +0900

    a

--- swaptest.c
+++ swaptest.c
@@ -15,7 +15,7 @@ int main () {
   printf(1, "%d %d\n", a, b);
 
   for(int i = 0; i < 100; i++){
-    sbrk(4096 * 4096);
+    sbrk(1024 * 1024);
     swapstat(&a, &b);
     printf(1, "%d %d\n", a, b);
   }
