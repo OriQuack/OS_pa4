51016869711b1f39482d5a4f26689d40ee0ec7ec
OriQuack <kimdoh0423@naver.com>
2024-05-22 22:00:01 +0900

    mytest

---
+++ mytest.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "fs.h"
+#include "proc.h"
+#include "syscall.h"
+
