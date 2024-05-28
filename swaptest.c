0c5cd93f99f2afffdd375c661b280b9f74415b0d
OriQuack <kimdoh0423@naver.com>
2024-05-28 18:30:22 +0900

    test

--- swaptest.c
+++ swaptest.c
@@ -18,7 +18,7 @@ int main () {
   char* mem = sbrk(4 * 1024 * 1024);
   mem[0] = 33;
 
-  for(int i = 0; i < 27; i++){
+  for(int i = 0; i < 54; i++){
     char* m = sbrk(4 * 1024 * 1024);
     m[0] = i;
   }
@@ -29,14 +29,14 @@ int main () {
     printf(1, "%d %d\n", a, b);
   }
 
-  if(fork() == 0){
-    printf(1, "CHILD: %d\n", mem[0]);
-    printf(1, "CHILD: %d\n", mem[4 * 1024 * 1024 * 4]);
-    swapstat(&a, &b);
-    printf(1, "%d %d\n", a, b);
-    printf(1, "CHILD EXIT\n");
-    exit();
-  }
+  // if(fork() == 0){
+  //   printf(1, "CHILD: %d\n", mem[0]);
+  //   printf(1, "CHILD: %d\n", mem[4 * 1024 * 1024 * 4]);
+  //   swapstat(&a, &b);
+  //   printf(1, "%d %d\n", a, b);
+  //   printf(1, "CHILD EXIT\n");
+  //   exit();
+  // }
 
   mem[0] = 3;
   swapstat(&a, &b);
@@ -48,8 +48,7 @@ int main () {
   swapstat(&a, &b);
   printf(1, "%d %d\n", a, b);
 
-  printf(1, "PROGRAM DONE\n");
-  wait();
-  printf(2, "WAIT DONE\n");
+  printf(1, "**PROGRAM DONE**\n");
+  // wait();
   exit();
 }
